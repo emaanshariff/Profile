515d064a05047b9d642b8719ed72ba334fa0b29d
emaanshariff <113933566+emaanshariff@users.noreply.github.com>
2023-12-01 23:51:27 -0500

    Update and rename streamlit_app.py to info.py

---
+++ info.py
@@ -0,0 +1,73 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/emaan.jpg"
+about_me = "I'm Emaan Shariff. I'm a full-time Computer Science student and partime Starbucks Barista. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/emaan-shariff-395b27209/"
+my_github_url = "https://github.com/emaanshariff"
+my_email_address = "eshariff3@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2027',
+    'GPA': '???'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1551"], 
+    "names":["Intro to CS", "Differential Calculus"], 
+    "semester_taken":["1st", "1st"],
+    "skills":["Python", "Basic Calculus (the first half)"],
+    }
+experience_data = {
+    "Tutor at Kumon Math & Reading Center": (["- Worked with kids to increase math and reading literacy",
+                                                                          "- Hated my job", "- Got paid $8/hr"],"images/kumon.jpeg"),
+    "Barista at Starbucks":(["- Brewed Coffee",
+                                                           "- Made coffee drinks for deranged millenials"],"images/starbucks.jpeg")
+
+}
+
+projects_data = {
+    "Slime Tutorial Web App (Phase 2)": "In this web application, the user can choose what slime type they want to make and how much of it they want to make and they'll be taught how to make it. Along with the option of learning how to make fake snow and activator!",
+}
+
+programming_data = {
+    "Python": 80,
+    "Java": 50,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+}
+spoken_icons = {"English": "🇬🇧",
+    "Urdu": "🇵🇰",
+    "Hindi":"🇮🇳"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Native",
+    "Urdu": "Fluent",
+    "Hindi": "Fluent",
+}
+leadership_data = {
+    "President of the Coding Club": (["- This is a lie"],"images/code.jpeg")
+
+}
+activity_data={
+    "Muslim Student Association (MSA)": (["- attend Islamic lectures", 
+            "- Mingle with fellow Muslim students", "- assist with planning and setting events up"])
+}
